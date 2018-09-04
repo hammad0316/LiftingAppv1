7d2f20f1a1763de167a04c556a7b952fc0c97324
Hammad Ahmad <hahmad0316@gmail.com>
2018-09-04 18:39:55 -0400

    Organized layout of files

--- client/src/App.js
+++ client/src/App.js
@@ -1,110 +1,19 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./components/homepage";
+import Table from "./pages/table";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      lifts: [],
-      muscleGroups: []
-    };
-    this.renderMuscleGroups = this.renderMuscleGroups.bind(this);
-    this.renderLifts = this.renderLifts.bind(this);
-    this.renderRows = this.renderRows.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/lifts.json')
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        this.setState({
-          lifts: data.lifts,
-          muscleGroups: data.muscle_groups,
-          loading: false
-        });
-      });
-  }
-
-  renderMuscleGroups() {
-    return (
-      <Fragment>
-        {this.state.muscleGroups.map(muscleGroup => (
-          <div key={muscleGroup.id}>
-            <h3>{muscleGroup.name}</h3>
-            <h5>{muscleGroup.area}</h5>
-          </div>
-        ))}
-      </Fragment>
-    );
-  }
-
-  renderLifts() {
-    return (
-      <Fragment>
-        {this.state.lifts.map(lift => {
-          return (
-            <div key={lift.id}>
-              <h3>{lift.name}</h3>
-              {lift.muscle_groups.map(mg => (
-                <li key={mg}>{mg}</li>
-              ))}
-            </div>
-          );
-        })}
-        ;
-      </Fragment>
-    );
-  }
-
-  renderRows() {
-    return (
-      <Fragment>
-        {this.state.lifts.map(lift => {
-          return (
-            <tbody key={lift.id}>
-              <tr>
-                <td>{lift.name}</td>
-                <td>
-                  {lift.muscle_groups.map(mg => (
-                    <li key={mg}>{mg}</li>
-                  ))}
-                </td>
-                <td>0</td>
-                <td>0</td>
-                <td>
-                  <button>Edit</button> <button>Delete</button>{' '}
-                </td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </Fragment>
-    );
-  }
-
   render() {
     return (
-      <Fragment>
-        {this.state.loading ? (
-          <h3>Loading...</h3>
-        ) : (
-          <table className="table">
-            <thead className="thead-dark">
-              <tr>
-                <th>Lift Name</th>
-                <th>Muscle Groups</th>
-                <th>Weight Put Up</th>
-                <th>Reps</th>
-                <th />
-              </tr>
-            </thead>
-            {this.renderRows()}
-          </table>
-        )}
-      </Fragment>
+      <Router>
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/table" component={Table} />}
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/homepage.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+import Table from "./pages/table";
+
+class HomePage extends Component {
+  render() {
+    return <Table />;
+  }
+}
+
+export default HomePage;
