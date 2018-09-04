d5e9306ccadda2b8401d54cd524d71215f9d6904
Hammad Ahmad <hahmad0316@gmail.com>
2018-09-04 16:18:32 -0400

    Was able to stop errors but still can't retrieve data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,47 +9,61 @@ class App extends Component {
       lifts: [],
       muscleGroups: []
     };
-
+    this.renderMuscleGroups = this.renderMuscleGroups.bind(this);
+    this.renderLifts = this.renderLifts.bind(this);
+    this.renderRows = this.renderRows.bind(this);
   }
 
   componentDidMount() {
-    fetch("/api/v1/lifts.json")
+    fetch("http://localhost:3000/api/v1/lifts.json")
       .then(response => {
         return response.json();
       })
-      .then(data => {
+      .then(lift=> {
         this.setState({
-          lifts: this.props.lifts,
-          muscleGroups: this.props.muscle_Groups
+          lifts: lift
         });
-      });
+      })
+      .then(muscleGroup=> {
+        this.setState({
+          muscleGroups: muscleGroup
+        });
+      })
   }
 
   renderMuscleGroups() {
-    return {this.state.muscleGroups.map(muscleGroup => {
-        <div key={muscleGroup.id}>
-          <h3>{muscleGroup.name}</h3>
-          <h5>{muscleGroup.area}</h5>
-        </div>
-    }})
+    return (
+      <Fragment>
+        {this.state.muscleGroups.map(muscleGroup => {
+            <div key={muscleGroup.id}>
+              <h3>{muscleGroup.name}</h3>
+              <h5>{muscleGroup.area}</h5>
+            </div>
+        })}
+      </Fragment>
+    )
   };
-  }
 
   renderLifts() {
-    return ({this.state.lifts.map(lift => {
-        <div key={lift.id}>
-          <h3>{lift.name}</h3>
-          {lift.muscle_groups.map(mg => (
-            <li key={mg}>{mg}</li>
-          ))}
-        </div>
-    }});
-  )
+    return (
+      <Fragment>
+      {this.state.lifts.map(lift => {
+        return (
+          <div key={lift.id}>
+            <h3>{lift.name}</h3>
+            {lift.muscle_groups.map(mg => (
+              <li key={mg}>{mg}</li>
+            ))}
+          </div>
+        );
+      })};
+      </Fragment>
+    );
   }
 
-    renderRows() {
+  renderRows() {
     return (
-      <React.fragment>
+      <Fragment>
         {this.state.lifts.map(lift => {
           return (
             <tbody key={lift.id}>
@@ -69,27 +83,26 @@ class App extends Component {
             </tbody>
           );
         })}
-      </React.fragment>
+      </Fragment>
     );
-  }
-
-
   }
 
   render() {
     return(
-      {<table className="table">
-        <thead className="thead-dark">
-          <th>Lift Name</th>
-          <th>Muscle Groups</th>
-          <th>Weight Put Up</th>
-          <th>Reps</th>
-          <th />
-        </thead>
-        {this.renderRows()}
-      </table>
-    });
-    }
+      <Fragment>
+        {<table className="table">
+          <thead className="thead-dark">
+            <th>Lift Name</th>
+            <th>Muscle Groups</th>
+            <th>Weight Put Up</th>
+            <th>Reps</th>
+            <th />
+          </thead>
+          {this.renderRows()}
+        </table>
+        }
+      </Fragment>);
   }
+}
 
 export default App;
