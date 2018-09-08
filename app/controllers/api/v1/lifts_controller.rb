05f4777ca3cb249ee3d3432b9e655bf2a595e441
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2018-09-08 17:22:21 -0400

    Updating seed file

--- app/controllers/api/v1/lifts_controller.rb
+++ app/controllers/api/v1/lifts_controller.rb
@@ -9,27 +9,29 @@ module Api::V1
               end
           end
 
-          @muscleGroups = MuscleGroup.all
+          render :json => {:lifts => @lifts}
 
-          @users = User.all
+          # @muscleGroups = MuscleGroup.all
 
-          @users.map do |user|
-              user.onerepmax.map! do |max|
-                max["lift"] = Lift.find(max["lift"]).name
-                max
-              end
-              user.lifts.map! do |userLift|
-                userLift = UserLift.find(userLift)
-                name = Lift.find(userLift.lift).name
-                lift = { "id": userLift.id, "name": name, "weight": userLift.weight, "reps": userLift.reps }
-                lift
-              end
-          end
+          # @users = User.all
+
+          # @users.map do |user|
+          #     user.onerepmax.map! do |max|
+          #       max["lift"] = Lift.find(max["lift"]).name
+          #       max
+          #     end
+          #     user.lifts.map! do |userLift|
+          #       userLift = UserLift.find(userLift)
+          #       name = Lift.find(userLift.lift).name
+          #       lift = { "id": userLift.id, "name": name, "weight": userLift.weight, "reps": userLift.reps }
+          #       lift
+          #     end
+          # end
 
 
-          render :json => {:lifts => @lifts,
-                           :muscle_groups => @muscleGroups,
-                           :users => @users}
+          # render :json => {:lifts => @lifts,
+          #                  :muscle_groups => @muscleGroups,
+          #                  :users => @users}
 
       end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,17 +22,43 @@ Lift.destroy_all
 
 benchpress = Lift.create name: 'Bench Press', muscle_groups: [chest.id, triceps.id]
 hammercurl = Lift.create name: 'Hammer Curl', muscle_groups: [biceps.id]
-squats = Lift.create name: 'Squats', muscle_groups: [quads.id, hamstring.id, glutes.id]
+squats = Lift.create name: 'Squats', muscle_groups: [quads.id, hamstrings.id, glutes.id]
+liftsArr = [benchpress, hammercurl, squats]
 
-UserLift.destroy_all
-
-lift1 = UserLift.create lift: benchpress.id, weight: 225, reps: 5
-lift2 = UserLift.create lift: hammercurl.id, weight: 40, reps: 5
-lift3 = UserLift.create lift: squat.id, weight: 225, reps: 5
+names = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", "Matthew", "Anthony", "Donald", "Mark", "Paul", "Steven", "Andrew", "Kenneth", "George", "Joshua", "Kevin", "Brian", "Edward", "Ronald", "Timothy", "Jason", "Jeffrey", "Ryan", "Jacob", "Gary", "Nicholas", "Eric", "Stephen", "Jonathan", "Larry", "Justin", "Scott", "Brandon", "Frank", "Benjamin", "Gregory", "Raymond", "Samuel", "Patrick", "Alexander", "Jack", "Dennis", "Jerry", "Tyler", "Aaron", "Henry", "Jose", "Douglas", "Peter", "Adam", "Nathan", "Zachary", "Walter", "Kyle", "Harold", "Carl", "Jeremy", "Gerald", "Keith", "Roger", "Arthur", "Terry", "Lawrence", "Sean", "Christian", "Ethan", "Austin", "Joe", "Albert", "Jesse", "Willie", "Billy", "Bryan", "Bruce", "Noah", "Jordan", "Dylan", "Ralph", "Roy", "Alan", "Wayne", "Eugene", "Juan", "Gabriel", "Louis", "Russell", "Randy", "Vincent", "Philip", "Logan", "Bobby", "Harry", "Johnny", "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Margaret", "Karen", "Nancy", "Lisa", "Betty", "Dorothy", "Sandra", "Ashley", "Kimberly", "Donna", "Emily", "Carol", "Michelle", "Amanda", "Melissa", "Deborah", "Stephanie", "Rebecca", "Laura", "Helen", "Sharon", "Cynthia", "Kathleen", "Amy", "Shirley", "Angela", "Anna", "Ruth", "Brenda", "Pamela", "Nicole", "Katherine", "Samantha", "Christine", "Catherine", "Virginia", "Debra", "Rachel", "Janet", "Emma", "Carolyn", "Maria", "Heather", "Diane", "Julie", "Joyce", "Evelyn", "Joan", "Victoria", "Kelly", "Christina", "Lauren", "Frances", "Martha", "Judith", "Cheryl", "Megan", "Andrea", "Olivia", "Ann", "Jean", "Alice", "Jacqueline", "Hannah", "Doris", "Kathryn", "Gloria", "Teresa", "Sara", "Janice", "Marie", "Julia", "Grace", "Judy", "Theresa", "Madison", "Beverly", "Denise", "Marilyn", "Amber", "Danielle", "Rose", "Brittany", "Diana", "Abigail", "Natalie", "Jane", "Lori", "Alexis", "Tiffany", "Kayla"]
+counter = 1;
 
 User.destroy_all
+UserLift.destroy_all
 
-User.create username: 'johnjacob', password: 'password', email: 'johnjacob@email.com', lifts: [lift1.id],
-  onerepmax: [{"lift": benchpress.id, "max": "1000"}, {"lift": hammercurl.id, "max": "300"}]
-User.create username: 'benfranklin', password: 'password', email: 'benfranklin@email.com', lifts: [],
-  onerepmax: [{"lift": benchpress.id, "max": "100"}, {"lift": hammercurl.id, "max": "60"}]
+while counter < 20 do
+  username = names.sample + counter.to_s
+  benchMax = (Random.rand(3) + 7) * 25
+  hammercurlMax = (Random.rand(3) + 7) * 25
+  newUser = User.create username: username, password_digest: 'password', email: username + '@email.com',
+    onerepmax: [{"lift": benchpress.id, "max": benchMax}, {"lift": hammercurl.id, "max": hammercurlMax}]
+
+  liftCounter = 0
+  while liftCounter < 20 do
+    
+    liftsArr.each do |lift|
+      weightLifted = Random.rand(8) * 25
+      repsDone = Random.rand(9) + 3
+      UserLift.create lift: lift.id, weight: weightLifted, reps: repsDone, user: newUser.id
+    end
+    
+    liftCounter  = liftCounter + 1
+  end
+
+  counter = counter + 1
+end
+
+# user1 = User.create username: 'johnjacob', password: 'password', email: 'johnjacob@email.com',
+# onerepmax: [{"lift": benchpress.id, "max": "1000"}, {"lift": hammercurl.id, "max": "300"}]
+# user2 = User.create username: 'benfranklin', password: 'password', email: 'benfranklin@email.com',
+# onerepmax: [{"lift": benchpress.id, "max": "100"}, {"lift": hammercurl.id, "max": "60"}]
+
+
+# lift1 = UserLift.create lift: benchpress.id, weight: 225, reps: 5
+# lift2 = UserLift.create lift: hammercurl.id, weight: 40, reps: 5
+# lift3 = UserLift.create lift: squat.id, weight: 225, reps: 5
